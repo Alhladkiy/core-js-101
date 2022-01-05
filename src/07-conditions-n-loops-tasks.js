ff0b0d977d516c5a6fbb3b20e0f9fd3e59f394bc
Alhladkiy <Alhladkiy@gmail.com>
2022-01-05 18:36:23 +0300

    feat: add function reverseString

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -210,8 +210,8 @@ function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return String(str.split('').reverse().join(''));
 }
 
 
