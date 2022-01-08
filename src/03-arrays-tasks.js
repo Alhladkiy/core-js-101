f8082549a494348881af7cbba105e13eb1c4342f
Alhladkiy <Alhladkiy@gmail.com>
2022-01-09 00:30:34 +0300

    feat: add function getIntervalArray

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -476,8 +476,9 @@ function getIdentityMatrix(/* n */) {
  *     0, 100 => [ 0, 1, 2, ..., 100 ]
  *     3, 3   => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  const arr = new Array(end - start + 1).fill(0);
+  return arr.map((item, index) => start + index);
 }
 
 /**
