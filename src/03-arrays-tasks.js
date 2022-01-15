5157d9c64de6b3edbe09aa751b259062fb81742b
Alhladkiy <Alhladkiy@gmail.com>
2022-01-15 18:37:56 +0300

    feat: add function swapHeadAndTail

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -584,8 +584,11 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const middle = Math.floor(arr.length / 2);
+  const head = arr.splice(0, middle);
+  const tail = arr.splice(arr.length - middle, middle);
+  return tail.concat(arr, head);
 }
 
 
